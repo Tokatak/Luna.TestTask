7823f13e04ab9054a9676e841b49d92e0e940445
Egor <egor.bashinsky@gmail.com>
2019-03-14 19:53:57 +0200

    Scene parser wizard

---
+++ Assets/Dependency/Wizard/Editor/SceneToHtmlWizard.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneToHtmlWizard : ScriptableWizard
+{
+    [MenuItem("Tools/ToHtmlWizard %t")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard<SceneToHtmlWizard>("Scene Parser", "Parse");
+    }
+
+    void OnWizardCreate()
+    {
+        var scene = SceneManager.GetActiveScene();
+        var path = scene.path;
+
+        var dataPath = Directory.GetParent(Application.dataPath).FullName;
+        var fullPath = Path.Combine(dataPath, path);
+        Debug.Log($"Scene path {fullPath}");
+
+        using (var stream = new StreamReader(fullPath))
+        {
+            var rawSceneContent = stream.ReadToEnd();
+            Debug.Log(rawSceneContent);
+        }
+    }
+}
