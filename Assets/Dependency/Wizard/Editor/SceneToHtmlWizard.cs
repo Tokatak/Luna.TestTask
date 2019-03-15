b837fd9856d17b85ff6507a1a33b3921cd73e89e
Egor <egor.bashinsky@gmail.com>
2019-03-15 15:07:28 +0200

    Scene to html unity wizard

--- Assets/Dependency/Wizard/Editor/SceneToHtmlWizard.cs
+++ Assets/Dependency/Wizard/Editor/SceneToHtmlWizard.cs
@@ -1,29 +1,39 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityYaml;
+using UnityYaml.Attributes;
+using YamlDotNet.Serialization;
+
 
 public class SceneToHtmlWizard : ScriptableWizard
 {
-    [MenuItem("Tools/ToHtmlWizard %t")]
+    [MenuItem("Tools/SceneToHtmlWizard %t")]
     static void CreateWizard()
     {
-        ScriptableWizard.DisplayWizard<SceneToHtmlWizard>("Scene Parser", "Parse");
+        DisplayWizard<SceneToHtmlWizard>("Scene Parser", "Parse");
     }
 
     void OnWizardCreate()
     {
         var scene = SceneManager.GetActiveScene();
-        var path = scene.path;
+        var rawSceneContent = new SceneReader().Read(scene);
+        var components = new SceneContentParser().Parse(rawSceneContent);
 
-        var dataPath = Directory.GetParent(Application.dataPath).FullName;
-        var fullPath = Path.Combine(dataPath, path);
-        Debug.Log($"Scene path {fullPath}");
+        IYamlUnityIdMapper typeMapper = new YamlUnityIdMapper(Constants.COMPONENT_WRAPPER_ASSEMBLY, Constants.COMPONENTS_NAMESPACE);
+        IComponentParser componentParser = new ComponentParser(typeMapper);
+        var parsedComponents = componentParser.Parse(components);
 
-        using (var stream = new StreamReader(fullPath))
+        foreach (object parsedComponent in parsedComponents)
         {
-            var rawSceneContent = stream.ReadToEnd();
-            Debug.Log(rawSceneContent);
+            Debug.Log(parsedComponent);
         }
     }
 }
+
+
