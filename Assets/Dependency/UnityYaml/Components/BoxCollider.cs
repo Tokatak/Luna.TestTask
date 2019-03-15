05a1d804deb0465c1fe369f8dc4a155b4f8efdb5
Egor <egor.bashinsky@gmail.com>
2019-03-15 20:51:37 +0200

    added missing components

---
+++ Assets/Dependency/UnityYaml/Components/BoxCollider.cs
@@ -0,0 +1,10 @@
+using UnityYaml.Attributes;
+
+namespace UnityYaml.Components
+{
+    [UnityYamlBinding(classId:65)]
+    public class BoxCollider
+    {
+        
+    }
+}
---
+++ Assets/Dependency/UnityYaml/Components/MeshRenderer.cs
@@ -0,0 +1,10 @@
+using UnityYaml.Attributes;
+
+namespace UnityYaml.Components
+{
+    [UnityYamlBinding(classId:23)]
+    public class MeshRenderer
+    {
+        
+    }
+}
