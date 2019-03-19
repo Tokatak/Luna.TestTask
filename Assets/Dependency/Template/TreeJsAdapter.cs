32e8b54926533f058a0339a834b39172d7219efd
Egor <egor.bashinsky@gmail.com>
2019-03-19 03:27:08 +0200

    bugix: Different rotation handling order for uinty(ZXY) and treejs(XYZ)

--- Assets/Dependency/Template/TreeJsAdapter.cs
+++ Assets/Dependency/Template/TreeJsAdapter.cs
@@ -72,14 +72,31 @@ public class TreeJsAdapter
         {
             var transform = component as Transform;
 
-            //Note other z destination
-            context.Container.Position = new[]
-                {transform.m_LocalPosition.x, transform.m_LocalPosition.y, -transform.m_LocalPosition.z};
+            //Note other z direction
+            context.Container.Position = new[]{transform.m_LocalPosition.x, transform.m_LocalPosition.y, - transform.m_LocalPosition.z};
+            
+            var orientation = new Quaternion(
+                transform.m_LocalRotation.x,
+                transform.m_LocalRotation.y,
+                transform.m_LocalRotation.z,
+                transform.m_LocalRotation.w);
+
+            var euler = orientation.eulerAngles;
+            //Note same direction
+            var xRot = Quaternion.AngleAxis(-euler.x,Vector3.right);
+            var yRot = Quaternion.AngleAxis(-euler.y, Vector3.up);
+            //Note different directions
+            var zRot = Quaternion.AngleAxis(-euler.z, Vector3.back);   
+                
+            var  XYZeuler = (xRot * yRot * zRot).eulerAngles;  
+                
             context.Container.Rotation = new[]
             {
-                transform.m_LocalEulerAnglesHint.x, transform.m_LocalEulerAnglesHint.y,
-                transform.m_LocalEulerAnglesHint.z
+                XYZeuler.x * Mathf.Deg2Rad,
+                XYZeuler.y * Mathf.Deg2Rad,
+                XYZeuler.z * Mathf.Deg2Rad
             };
+            
             context.Container.Scale = new[]
                 {transform.m_LocalScale.x, transform.m_LocalScale.y, transform.m_LocalScale.z};
         }
