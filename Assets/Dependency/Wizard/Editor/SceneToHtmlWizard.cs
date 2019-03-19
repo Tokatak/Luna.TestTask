facb43c85dc67aa21cefa86bd31c3e9ea6f6f5cb
Egor <egor.bashinsky@gmail.com>
2019-03-19 05:41:28 +0200

    cleanup

--- Assets/Dependency/Wizard/Editor/SceneToHtmlWizard.cs
+++ Assets/Dependency/Wizard/Editor/SceneToHtmlWizard.cs
@@ -33,21 +33,22 @@ public class SceneToHtmlWizard : ScriptableWizard
         var scene = SceneManager.GetActiveScene();
         var sceneContent = new SceneReader().Read(scene);
         var rawYamlComponents = new SceneContentParser().Parse(sceneContent);
+        
         var typeMapper = new YamlUnityIdMapper(Constants.COMPONENTS_WRAPPER_ASSEMBLY, Constants.COMPONENTS_NAMESPACE);
         var componentParser = new YamlDotNetComponentParser(typeMapper);
-        var components = componentParser.Parse(rawYamlComponents);
+        
+        var parsedComponents = componentParser.Parse(rawYamlComponents);
+        var sceneData = new SceneData(parsedComponents);
      
         //deply template
         var deployer = new Deployer(BuildDestinationFolder);
         string deployPath =  deployer.Deploy(Path.Combine(Application.dataPath +TemplatePath));
         var pathToIndex =  Path.Combine(deployPath ,@"index.html");
-        
-        var sceneData = new SceneData(components);
         var pathToContent = Path.Combine(deployPath, TemplateContentPath);
         
+        //TreeJS
         var treeJsAdapterMapper = new YamlToTreeJSComponentMapper(Constants.TREEJS_ASSEMBLY,Constants.TREEJS_ADAPTERS_NAMESPAGE);
         new TreeJsAdapter(treeJsAdapterMapper).DeployContent(sceneData, pathToContent);
-        
         
         //point explorer to index file
 //        EditorUtility.RevealInFinder(pathToIndex);
